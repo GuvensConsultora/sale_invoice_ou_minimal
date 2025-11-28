529e126417dbf75ac8074e0f69bd59beb3f823e2
guvensconsultora <guvensconsultora@gmail.com>
2025-11-28 12:33:10 -0300

    Corrección de la importación agregando _

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,6 +1,6 @@
 # models/sale_order.py
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
 class SaleOrder(models.Model):
